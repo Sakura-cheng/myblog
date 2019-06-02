bb89ff4f1849a7aa7c92cf6989ebf049e79a9b8f
ljc <sakura_ljc@163.com>
2019-06-02 08:07:18 +0800

    本地化时间

--- myblog/models.py
+++ myblog/models.py
@@ -9,7 +9,7 @@ class Admin(db.Model, UserMixin):
     username = db.Column(db.String(20))
     password_hash = db.Column(db.String(128))
     blog_title = db.Column(db.String(60))
-    bolg_sub_title = db.Column(db.String(100))
+    blog_sub_title = db.Column(db.String(100))
     name = db.Column(db.String(30))
     about = db.Column(db.Text)
 
